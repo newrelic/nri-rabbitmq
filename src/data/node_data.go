897eb4bac17e910323dfcf2128c30b192c6c8371
Shannon Mitchell <shannon.mitchell@rackspace.com>
2020-10-16 10:58:06 -0500

    Adding additional node data metrics

--- src/data/node_data.go
+++ src/data/node_data.go
@@ -10,17 +10,21 @@ import (
 
 // NodeData is the representation of the nodes endpoint
 type NodeData struct {
-	Name                string
-	ConfigFiles         []string `json:"config_files"`
-	DiskAlarm           *bool    `json:"disk_free_alarm" metric_name:"node.diskAlarm" source_type:"gauge"`
-	DiskFreeSpace       *int64   `json:"disk_free" metric_name:"node.diskSpaceFreeInBytes" source_type:"gauge"`
-	FileDescriptorsUsed *int64   `json:"fd_used" metric_name:"node.fileDescriptorsTotalUsed" source_type:"gauge"`
-	MemoryAlarm         *bool    `json:"mem_alarm" metric_name:"node.hostMemoryAlarm" source_type:"gauge"`
-	MemoryUsed          *int64   `json:"mem_used" metric_name:"node.totalMemoryUsedInBytes" source_type:"gauge"`
-	Partitions          int      `json:"-" metric_name:"node.partitionsSeen" source_type:"gauge"`
-	Running             *bool    `metric_name:"node.running" source_type:"gauge"`
-	RunQueue            *int64   `json:"run_queue" metric_name:"node.averageErlangProcessesWaiting" source_type:"gauge"`
-	SocketsUsed         *int64   `json:"sockets_used" metric_name:"node.fileDescriptorsUsedSockets" source_type:"gauge"`
+	Name                 string
+	ConfigFiles          []string `json:"config_files"`
+	DiskAlarm            *bool    `json:"disk_free_alarm" metric_name:"node.diskAlarm" source_type:"gauge"`
+	DiskFreeSpace        *int64   `json:"disk_free" metric_name:"node.diskSpaceFreeInBytes" source_type:"gauge"`
+	FileDescriptorsUsed  *int64   `json:"fd_used" metric_name:"node.fileDescriptorsTotalUsed" source_type:"gauge"`
+	FileDescriptorsTotal *int64   `json:"fd_total" metric_name:"node.fileDescriptorsTotal" source_type:"gauge"`
+	ProcessesTotal       *int64   `json:"proc_total" metric_name:"node.processesTotal" source_type:"gauge"`
+	ProcessesUsed        *int64   `json:"proc_used" metric_name:"node.processesUsed" source_type:"gauge"`
+	MemoryAlarm          *bool    `json:"mem_alarm" metric_name:"node.hostMemoryAlarm" source_type:"gauge"`
+	MemoryUsed           *int64   `json:"mem_used" metric_name:"node.totalMemoryUsedInBytes" source_type:"gauge"`
+	Partitions           int      `json:"-" metric_name:"node.partitionsSeen" source_type:"gauge"`
+	Running              *bool    `metric_name:"node.running" source_type:"gauge"`
+	RunQueue             *int64   `json:"run_queue" metric_name:"node.averageErlangProcessesWaiting" source_type:"gauge"`
+	SocketsTotal         *int64   `json:"sockets_total" metric_name:"node.fileDescriptorsTotalSockets" source_type:"gauge"`
+	SocketsUsed          *int64   `json:"sockets_used" metric_name:"node.fileDescriptorsUsedSockets" source_type:"gauge"`
 }
 
 // GetEntity creates an integration.Entity for this NodeData
